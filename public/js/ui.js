e4e2bf50c283f07cfb95f9044e69a7b7a0f0dace
Donald Chea <donaldducky@gmail.com>
2013-12-15 14:06:36 -0500

    Tweak more css

--- public/js/ui.js
+++ public/js/ui.js
@@ -5,8 +5,8 @@ define([
 
   var ui = {
     selectWeapon: function(element) {
-      $('#weapons .icon').removeClass('selected');
-      $('#weapons .icon-' + element).addClass('selected');
+      $('#weapons .icon-container').removeClass('selected');
+      $('#weapons .icon-' + element).parent().addClass('selected');
     }
   };
 
